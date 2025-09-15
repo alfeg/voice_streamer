d229a39be27399e4465daf0c605ff5dfea6faa99
Victor Gladkikh <victor.gladkikh@gmail.com>
2025-09-15 19:49:54 +0300

    Don't stop streaming on error

--- VoiceStreamer/VoiceStreamerClient.cs
+++ VoiceStreamer/VoiceStreamerClient.cs
@@ -24,46 +24,49 @@ public class VoiceStreamerClient(IOptions<StreamConfig> config, ChannelReader<Vo
 
     private async Task StartContinuousStream(CancellationToken cancellationToken)
     {
-        var stream = new PcmStream(_oggFileChannel, config.Value, log, _cts.Token);
-        var audioSource = new StreamPipeSource(stream);
-
-        try
+        while (!cancellationToken.IsCancellationRequested)
         {
-            _log.Information("Старт стриминга в {RtmpServerUrl}{StreamingKey}", config.Value.RtmpServerUrl, config.Value.RtmpStreamKey?.Obfuscate());
-            
-            // FFmpeg command: ffmpeg -f s16le -ar 44100 -ac 2 -i pipe:0 -c:a aac -ab 128k -f flv -re rtmp://...
-            var arguments = FFMpegArguments
-                    .FromPipeInput(audioSource, options => options
-                        .WithCustomArgument($"-re -f s16le -ar {PcmStream.SampleRate} -ac {PcmStream.Channels}")
-                    )
-                    .OutputToUrl(config.Value.GetUrl(), options => options
+            var stream = new PcmStream(_oggFileChannel, config.Value, log, _cts.Token);
+            var audioSource = new StreamPipeSource(stream);
+
+            try
+            {
+                _log.Information("Старт стриминга в {RtmpServerUrl}{StreamingKey}", config.Value.RtmpServerUrl,
+                    config.Value.RtmpStreamKey?.Obfuscate());
+
+                // FFmpeg command: ffmpeg -f s16le -ar 44100 -ac 2 -i pipe:0 -c:a aac -ab 128k -f flv -re rtmp://...
+                var arguments = FFMpegArguments
+                        .FromPipeInput(audioSource, options => options
+                            .WithCustomArgument($"-re -f s16le -ar {PcmStream.SampleRate} -ac {PcmStream.Channels}")
+                        )
+                        .OutputToUrl(config.Value.GetUrl(), options => options
                             .WithAudioCodec("aac")
                             .WithAudioBitrate(128)
                             .ForceFormat("flv")
-                    )
-                    .WithLogLevel(FFMpegCore.Enums.FFMpegLogLevel.Info)
-                    .NotifyOnOutput(Console.WriteLine)
-                  //.NotifyOnError(_log.ForContext("Module", "[VF] ffpmpeg: ").Information)
-                ;
+                        )
+                        .WithLogLevel(FFMpegCore.Enums.FFMpegLogLevel.Info)
+                        .NotifyOnOutput(Console.WriteLine)
+                    //.NotifyOnError(_log.ForContext("Module", "[VF] ffpmpeg: ").Information)
+                    ;
 
-            var taSource = new TaskCompletionSource();
-            cancellationToken.Register(() => taSource.SetCanceled(cancellationToken));
-            
-            await Task.WhenAny(
-                taSource.Task,
-                arguments.ProcessAsynchronously()
-            );
-            
-            _log.Information("Остановка стриминга");
-        }
-        catch (OperationCanceledException)
-        {
-            _log.Information("Отмена стриминга");
-        }
-        catch (Exception ex)
-        {
-            _log.Error(ex, "Ошибка стриминга. Аварийный выход");
-            Environment.Exit(1);
+                var taSource = new TaskCompletionSource();
+                cancellationToken.Register(() => taSource.SetCanceled(cancellationToken));
+
+                await Task.WhenAny(
+                    taSource.Task,
+                    arguments.ProcessAsynchronously()
+                );
+
+                _log.Information("Остановка стриминга");
+            }
+            catch (OperationCanceledException oce)
+            {
+                _log.Error(oce , "Отмена стриминга");
+            }
+            catch (Exception ex)
+            {
+                _log.Error(ex, "Ошибка стриминга. Аварийный выход");
+            }
         }
     }
 
